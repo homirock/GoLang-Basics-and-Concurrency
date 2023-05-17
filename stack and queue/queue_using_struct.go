4dd5a56c295bd90ae72110634901b229246924d4
pgiri <parthsarathigiri@gmail.com>
2023-05-18 01:47:00 +0530

    queue

--- stack and queue/queue_using_struct.go
+++ stack and queue/queue_using_struct.go
@@ -1,1 +1,34 @@
 package main
+
+import "fmt"
+
+type queue struct {
+	elements []int
+	size     int
+}
+
+func (m *queue) push(element int) {
+	if len((*m).elements) == ((*m).size) {
+		return
+	}
+	(*m).elements = append((*m).elements, element)
+
+}
+func (m *queue) pop() {
+	if len((*m).elements) == 0 {
+		return
+	}
+	(*m).elements = (*m).elements[1:]
+}
+
+func main() {
+	q := queue{size: 3}
+	q.push(1)
+	fmt.Println(q.elements)
+	q.push(2)
+	fmt.Println(q.elements)
+	q.push(3)
+	fmt.Println(q.elements)
+	q.pop()
+	fmt.Println(q.elements)
+}
