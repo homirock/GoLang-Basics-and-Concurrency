8c456c9698bd6886888266f8e9e5827249288267
pgiri <parthsarathigiri@gmail.com>
2023-07-19 00:49:33 +0530

    addition of 2 numbers

--- concurrency/Program/oddeven_sequence_new.go
+++ concurrency/Program/oddeven_sequence_new.go
@@ -1,24 +1,22 @@
 package main
 
-
-
 func main() {
-	ch:=make(chan int)
+	ch := make(chan int)
 
-	go func(){
-		for i:=0;i<10;i++{
-			ch<-i
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
 		}
 	}()
-	status:=true
-	for i:=0;i<10;i++{
-		val<-ch
-		if val%2==0&& status{
-			fmt.Println("even:",val)
-			status=false
-		}else{
-			fmt.Println("odd:",val)
-			status=true
+	status := true
+	for i := 0; i < 10; i++ {
+		val <- ch
+		if val%2 == 0 && status {
+			fmt.Println("even:", val)
+			status = false
+		} else {
+			fmt.Println("odd:", val)
+			status = true
 		}
 	}
 }
