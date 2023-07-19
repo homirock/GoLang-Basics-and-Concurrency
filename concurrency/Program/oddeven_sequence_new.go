6fb51361f11a229db22bc4f7762ee5635329bc03
pgiri <parthsarathigiri@gmail.com>
2023-07-20 00:47:19 +0530

    odd even in sequence using 1 go routine

--- concurrency/Program/oddeven_sequence_new.go
+++ concurrency/Program/oddeven_sequence_new.go
@@ -1,1 +1,24 @@
 package main
+
+
+
+func main() {
+	ch:=make(chan int)
+
+	go func(){
+		for i:=0;i<10;i++{
+			ch<-i
+		}
+	}()
+	status:=true
+	for i:=0;i<10;i++{
+		val<-ch
+		if val%2==0&& status{
+			fmt.Println("even:",val)
+			status=false
+		}else{
+			fmt.Println("odd:",val)
+			status=true
+		}
+	}
+}
